1603871710c52694301632abff88ba773f63b4a1
BrendenGreeff <brendengreeffie@gmail.com>
2021-11-21 09:04:34 +0200

    First commit after resarting project last minute

---
+++ Project2Cmpg323/DataLibrary/DataAccess.cs
@@ -0,0 +1,44 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataLibrary
+{
+    public class DataAccess : IDataAccess
+    {
+        public DataAccess getSavedDataAccess()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                var rows = await connection.QueryAsync<T>(sql, parameters);
+                return rows.ToList();
+            }
+        }
+
+        public DataAccess Save(DataAccess users)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveData<T>(string sql, T parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                return connection.ExecuteAsync(sql, parameters);
+
+            }
+        }
+
+
+    }
+}
---
+++ Project2Cmpg323/Project2Cmpg323/Models/ImagesModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectCmpg323.Models
+{
+    public class ImagesModels
+    {
+        [Key]
+        public int Images_ID { get; set; }
+        public string Images_Name { get; set; }
+        public byte[] Images_Path { get; set; }
+        public string Images_Username { get; set; }
+
+    }
+}
